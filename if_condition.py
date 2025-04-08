20438a360001e369e76091b4e64f70b2f3215eac
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-04-08 14:33:54 +0530

    Add files via upload

---
+++ if_condition.py
@@ -0,0 +1,23 @@
+#1
+#x = 10
+#if x > 5 :
+  #    print("x is greater than 5")
+
+#Basic structure
+#if condition:
+      #code to run if condition is True
+
+#
+x=10
+if x>10:
+  print('x is greater than 10')
+elif x==10:
+  print('x is exactly equal to 10')
+else:
+   print('x is less than 10')
+
+#x=10
+#y=5
+
+#if x>5 and y<10:
+ #  print("both condition are true")
