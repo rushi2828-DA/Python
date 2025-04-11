47d896304ef738aa248c7802cff2e881ef73862a
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-04-11 15:34:06 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,31 @@
+# Define two numbers
+a=15
+b=4
+
+#Addition
+addition =a+b
+print(f"addition: {a}+{b}= {addition}")
+
+#Substraction
+substraction=a-b
+print(f"substraction:{a} - {b} = {substraction}")
+
+#Multiplication
+multiplication=a*b
+print(f"multiplication: {a} * {b} =  {multiplication}")
+
+#Division (floating-point)
+division= a/b
+print(f"division: {a} / {b} ={division}")
+
+#Floor division(integer division)
+floor_division =a//b
+print(f"floor_division: {a} // {b}= {floor_division}")
+
+#Modulus (Reminder)
+modulus= a%b 
+print(f"modulus:  {a} % {b}= {modulus}")
+
+#Exponentiation (Power)
+exponentiation = a ** b
+print(f"exponentiation: {a} ** {b}= {exponentiation}")
