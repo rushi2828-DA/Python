2a1361446ccfa3d7f5f1c46b551095f6f70ed4d9
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-04-12 16:50:53 +0530

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,35 @@
+a = 10
+
+#Assign
+print(f"initial value: a ={a}")
+
+#Add and assign
+a += 5 #equivalent to: a=a+ 5
+print(f"after a += 5 : {a}")
+
+#substract and assiugn
+a -=3 #equivalent to : a -3
+print(f"after a-=3 : {a}")
+
+#Multiply and assign 
+a *=2 #equivalent to a = a* 2
+print(f"after a *=2: {a}")
+
+#Divide and assign 
+a /= 4 #equivalent to : a = a/4
+print(f"after a/ = 4: {a}")
+
+#floor divide and assign 
+a //= 2 #equivalent a = a//2
+print(f"after a//=2:{a}")
+
+# Modulus and assign
+a %= 3  # Equivalent to: a = a % 3
+print(f"After a %= 3: {a}")
+
+# Exponentiation and assign
+a *= 2  # Equivalent to: a = a * 2
+print(f"After a **= 2: {a}")
+
+
+
