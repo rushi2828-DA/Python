cb25e0f77ccf965928aab780f0e486d2ba4ea937
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-04-16 10:23:50 +0530

    Add files via upload

---
+++ comparsion.py
@@ -0,0 +1,25 @@
+# Define two numbers
+a = 10
+b = 20
+
+print("a =", a)
+print("b =", b)
+
+# Equal to (==)
+print("a == b:", a == b)  # False
+
+# Not equal to (!=)
+print("a != b:", a != b)  # True
+
+# Greater than (>)
+print("a > b:", a > b)    # False
+
+# Less than (<)
+print("a < b:", a < b)    # True
+
+# Greater than or equal to (>=)
+print("a >= b:", a >= b)  # False
+
+# Less than or equal to (<=)
+print("a <= b:", a <= b)  # True
+
