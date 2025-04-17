0ce50e2087828fe5aeec2fb7e13918a2dcd1470e
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-04-17 09:53:37 +0530

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,16 @@
+# Variables for demonstration
+a = True
+b = False
+
+print("a =", a)
+print("b =", b)
+
+# Logical AND
+print("a and b:", a and b)  # False (Both need to be True)
+
+# Logical OR
+print("a or b:", a or b)    # True (At least one needs to be True)
+
+# Logical NOT
+print("not a:", not a)      # False (Negates the value of a)
+print("not b:", not b)      # True (Negates the value of b)
