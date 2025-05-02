e17c1e93ed9ee23d2905f19e4d3b4fb4c17130ed
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-05-02 11:38:13 +0530

    Add files via upload

---
+++ lists as array.py
@@ -0,0 +1,16 @@
+#creating a list(array)
+arr = [1,2,3,4,5]
+
+# Basic lists operations
+arr.append(6)  #add element at the end
+arr.insert(2,10) # Insert 10 at index 2
+arr.remove(3) # Remove first occurance of 3
+arr.pop()  #remove and return last element
+arr.reverse() #reverse the list
+arr.sort() #sort the list (ascending by default)
+
+# Accessing Elements
+print(arr[0]) #first element
+print(arr[-1]) #last element
+print(arr[1:4]) #slice from index 1 to 3
+
