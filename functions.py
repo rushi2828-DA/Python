fd9200add1d53f0fe9cae2a7c743af51a3749cfb
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-05-09 10:33:01 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,43 @@
+def add (a,b):
+    return a+b
+result=(10 + 6)
+print(result)
+
+
+def substract(a,b):
+    return a-b
+result=(2-4)
+print(result)
+
+def multiplication (a,b):
+    return(a*b)
+
+result=(15*5)
+print(result)
+
+
+def division(a,b):
+    return (a/b)
+
+result=(50/4)
+print(result)
+
+#even odd
+def even_odd(num):
+    if num%2==0:
+        return f"{num}is even",
+    else :
+        return f"{num}is odd"
+print(even_odd(13))
+
+#greet by name 
+def greet(name):
+    return f"Hey,{name}!,good to see u"
+print(greet('Joshua'))
+
+#welcome
+def greet():
+   print("Hello, welcome Siddarth")
+greet()
+            
+            
