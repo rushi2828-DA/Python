04f581c5c7381c8c715e52cc7b7a60c37ddf0ba9
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-07-04 13:42:58 +0530

    Add files via upload

---
+++ example _1.py
@@ -0,0 +1,15 @@
+#add 10 to argument a,and return the result
+x=lambda a:a+10 
+print(x(5))
+
+# substract 3 from argument a, and return the result
+y=lambda a:a-3
+print(y(13))
+
+# multiply 4 from argument a, and return the result
+z=lambda a:a*4
+print(z(4))
+
+# divide from argument a, and return the result
+d=lambda a:a/5
+print(d(20))
