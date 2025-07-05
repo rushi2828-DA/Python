6f11e458db98289d603231b3cbeaa61f576c9e7d
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-07-05 15:28:21 +0530

    Add files via upload

---
+++ example_2.py
@@ -0,0 +1,17 @@
+#Multiply argument a with argument b and return the result:
+w=lambda a,b:a*b
+print(w(5,3))
+
+#addition argument a with argument b and return the result:
+x=lambda a,b:a+b
+print(x(10,20))
+
+#substract argument a with argument b and return the result:
+y= lambda a,b:a-b
+print(y(5,3))
+
+#divide argument a with argument b and return the result:
+z=lambda a,b:a/b
+print(z(10,2))
+
+
