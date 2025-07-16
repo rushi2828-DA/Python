08f53b9d1b17b196379f17bd4b7078c04546f2cc
rushi2828-DA <rushikeshpatil6154@gmail.com>
2025-07-16 14:46:25 +0530

    Add files via upload

---
+++ Barplot.py
@@ -0,0 +1,16 @@
+#types of plot in pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data={
+    'Month': ['Jan','Feb','Mar','Apr','May'],
+    'Sales': [200,250,300,280,320],
+    'Profit': [50,70,90,85,95]
+}
+
+df=pd.DataFrame(data)
+#Bar plot
+df.plot(x='Month',y='Sales',kind='bar',color='Skyblue')
+plt.title('Monthly Sales - Bar Plot')
+plt.show()
